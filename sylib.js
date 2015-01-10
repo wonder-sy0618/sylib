2dbbafbe501bc192bf08048e1e3416dcabdff6af
shiying_in_home <wonder_sy0618@foxmail.com>
2015-01-10 20:55:09 +0800

    add cookie utils

--- sylib.js
+++ sylib.js
@@ -87,6 +87,7 @@ seajs.config({
 		'jquery' : 			'jquery/jquery/1.10.1/jquery-debug.js',
 		'jquery/jquery' : 	'jquery/jquery/1.10.1/jquery-debug.js',
 		'jquery/easyui' : 	'jquery/easyui/jquery.easyui.min.js',
+		'jquery/cookie' : 	'jquery/cookie/jquery.cookie.js',
 		'art/template' : 	'art/template/template.js',
 		'art/artdialog' : 	'art/artdialog/src/dialog-plus.js',
 		'moment/moment': 	'moment/moment/moment.js',
